edaba436706dce03accfc5b5b58c9b3078d45ea3
Miguel Ángel <20430676+supermavster@users.noreply.github.com>
2020-05-18 08:40:35 -0500

    [Complete] Challenge

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 # Resolve the problem!!
-
+import re
 
 def run():
     # Start coding here
+    with open('encoded.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+    response = re.sub(r'[^\x00-\x7F]+','', text)
+    print(response)
 
 
 if __name__ == '__main__':
